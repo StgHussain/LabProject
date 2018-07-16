5e2cbfbbdf813f070d79e484f801c707c2e08f45
spoderman1 <thussain1123@gmail.com>
2018-07-16 22:54:49 +0200

    LgHologram

---
+++ LgHologram.py
@@ -0,0 +1,24 @@
+import numpy as np
+import scipy as scipy
+import math
+
+class LGHologram ():
+
+    def Laguerre(self, p, l, x):
+        y = np.array(p + 1, 1)
+        if p == 0:
+            y = 1
+        else:
+            for m in range (p):
+                factM1 = math.factorial(p + 1)
+                numerator = (np.power(-1, m)) * factM1
+                factPM = math.factorial(p-m)
+                factLM = math.factorial(l+m)
+                factM2 = math.factorial(m)
+                denom = factPm * factLM * factM2
+                y = numerator/denom 
+
+    def __init__(self):
+        self.p = 1
+        self.l = 0
+
