7612098d3af0153f111eac1bc16cb979391e0891
spoderman1 <thussain1123@gmail.com>
2018-07-17 14:32:46 +0200

    LgBeam
    LgBeam is implemented, testing required

--- LgBeam.py
+++ LgBeam.py
@@ -6,10 +6,16 @@ from Laguerre import Laguerre
 class LgBeam():
 
     def GenerateLGBeam(self, p, l, w, xx, yy):
+        Pi = math.pi
+        factP = math.factorial(p)
+        factLP = math.factorial(abs(l) + p)
+        factL = math.factorial (l)
         [rho, phi] = self.cart2pol(xx, yy)
         RhoSquaredOverWSquare = (rho*rho)/(w*w)
         lag = Laguerre()
         LagResult = lag.LaguerreBeam(p, abs(l), 2*RhoSquaredOverWSquare)
+        Clg = math.sqrt((2*factP/ (Pi * factLP))) / w
+        Result = Clg * (self.Square2 * np.power(math.sqrt(RhoSquaredOverWSquare), abs(l))) * LagResult * math.exp(-RhoSquaredOverWSquare) * exp(j * l * phi)
         
 
     def cart2pol(self, x, y):
@@ -19,3 +25,4 @@ class LgBeam():
 
     def __init__(self):
         self.x = 0
+        self.Square2 = math.sqrt(2)
