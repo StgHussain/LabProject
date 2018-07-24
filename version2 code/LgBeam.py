96e7f75eaaf621848ca3a746f836d4d64d039f91
spoderman1 <thussain1123@gmail.com>
2018-07-24 16:33:39 +0200

    done but inverted colours

--- version2 code/LgBeam.py
+++ version2 code/LgBeam.py
@@ -8,51 +8,76 @@ from Laguerre import Laguerre
 class LgBeam():
 
     def GenerateLGBeam(self, p, l, w):
-        
-        XXcords = np.linspace(-1, 1, 5)
-        YYcords = np.linspace(-1/1, 1/1, 5) #range/something (both need changing)
+        sizePoints = 5
+        XXcords = np.linspace(-1, 1, sizePoints)
+        YYcords = np.linspace(-1/1, 1/1,  sizePoints) #range/something (both need changing)
         Xcords, Ycords = np.meshgrid(XXcords, YYcords)
-        print (Xcords)
-        print (Ycords)
+        #print (Xcords)
+        #print (Ycords)
         print("co ordinates done")
 
         [rho, phi] = self.cart2pol(Xcords, Ycords)
         RhoSquaredOverWSquare = (rho*rho)/(w*w)
-        print(RhoSquaredOverWSquare)
+        #print(RhoSquaredOverWSquare)
         print("next")
         
         Values = self.LaguerreBeam(p,l, 2*RhoSquaredOverWSquare)
-        print(Values)
+        #print(Values)
         print("values")
 
         factP = math.factorial(p)
         factLP = math.factorial(abs(l) + p)
         Clg = math.sqrt((2*factP/ (self.PI * factLP))) / w
 
-        n = 5 #change this to match grid sizeS
+        n = sizePoints #change this to match grid sizeS
         Result = [[0]*n for x in range(n)] #n value is the size of the result matrix i.e the grid
-        for i in range (5):
-            for j in range (5):
+        for i in range (n):
+            for j in range (n):
                 imagNum = complex(0,-l*phi[i][j])
                 reqVal = Values[i][j]
                 Result[i][j] = Clg * (self.SquareRoot2 * np.power(math.sqrt(RhoSquaredOverWSquare[i][j]), abs(l))) * reqVal * np.exp(-RhoSquaredOverWSquare[i][j]) * np.exp(imagNum)
         gratingAngle = 45
         gratNum = 50
         print('results')
-        print(Result)
-        self.addGrating(Result, gratingAngle, gratNum)
+        #print(Result)
+
+        ResultNew = [np.abs(number) for number in Result]
+        maxResult = 0
+        for i in range(n):
+            maxVal = max(ResultNew[i])
+            if maxVal > maxResult:
+                maxResult = maxVal
+            #print(maxVal)
+        #print(maxResult)
+        ResultNew = ResultNew/maxResult
+        print("result absolute")
+        #print(ResultNew)
+        Phi = np.angle(Result)
+        print("phi values")
+        #print(Phi)
+        imaginaryNum = complex(0, 1)
+        complexHologram = [[0]*n for x in range(n)]
+        for a in range(n):
+            for b in range(n):
+                complexHologram[a][b] = ResultNew[a][b] * np.exp(imaginaryNum*Phi[a][b])
+        print("complex hologram")
+        #print(complexHologram)
+        self.addGrating(complexHologram, gratingAngle, gratNum)
 
 
     def addGrating(self, inputHologram, gratingAngle, gratingNum):
-        
+        sizePoints = 5
         print("add grating")
-        XXcords = np.linspace(-self.PI, self.PI, 5)
-        YYcords = np.linspace(-self.PI, self.PI, 5)
+        XXcords = np.linspace(-self.PI, self.PI, sizePoints)
+        YYcords = np.linspace(-self.PI, self.PI, sizePoints)
+
+        print("input hologram")
+        #print(inputHologram)
 
         print("xcords")
-        print(XXcords)
+        #print(XXcords)
         print("ycords")
-        print(YYcords)
+        #print(YYcords)
         print("correct co ords")
 
         Xcords, Ycords = np.meshgrid(XXcords, YYcords)
@@ -60,14 +85,14 @@ class LgBeam():
         theta = (self.PI/180)* gratingAngle
         plane = math.sin(theta)*Xcords + math.cos(theta)*Ycords
         print("plane")
-        print(plane)
+        #print(plane)
         phase = np.angle(inputHologram)
         print("phase")
-        print(phase)
+        #print(phase)
 
         phaseHologram = np.mod(phase + gratingNum*plane, 2*self.PI) - self.PI
         print("phase hologram")
-        print(phaseHologram)
+        #print(phaseHologram)
         intensity = np.abs(inputHologram)
 
         phaseHologram = phaseHologram * intensity
@@ -76,9 +101,10 @@ class LgBeam():
 
     def showImg(self, img):
         print("show image")
-        sizeIm = len(img)
-        print(sizeIm)
-        plt.imshow(img, aspect = 'auto', cmap = plt.get_cmap('binary'))
+        finalMat = [*zip(*img)]
+        #print(finalMat)
+        plt.matshow(finalMat, aspect = 'auto', cmap = plt.get_cmap('greys'))
+        plt.show()
     
     def LaguerreBeam(self, p, l, x):
         #self.y = np.array(p + 1, 1)
@@ -100,7 +126,7 @@ class LgBeam():
         #polyVal if needed
         PolyCoeff = np.polyval(Vals, x)
         print("poly coeff")
-        print(PolyCoeff)
+        #print(PolyCoeff)
         return PolyCoeff
 
     def cart2pol(self, x, y):
