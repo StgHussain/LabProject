1c2fa0112766cbff6d080b493934e42962b38f2a
spoderman1 <thussain1123@gmail.com>
2018-08-09 18:59:47 +0200

    Basic UI

--- v3 python/ControlScript.py
+++ v3 python/ControlScript.py
@@ -32,17 +32,54 @@ class MainPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-
         label = tk.Label(self, text ="Hologram Generation")
-        label.grid(row = 0, padx = 30)
+        label.grid(row = 0, padx = 50)
 
         label = tk.Label(self, text ="Enter number of Beams required")
         label.grid(row = 1, column = 0)
+
+        self.beamNums = {'beam one', "beam two", 'beam three'}
 
         self.totalBeamNum = tk.StringVar()
         myEntry = tk.Entry(self, textvariable = self.totalBeamNum)
         myEntry.grid(row=1, column=2, padx = 10, pady = 10, sticky = "E")
 
+    #Drop down menu for beam number, set Beam num to the structs and number of beams the user wants
+    ############################################################################
+        BeamNum = ['Beam 1', 'Beam 2', 'Beam 3']
+        self.Beam = tk.StringVar()
+        self.Beam.set("Beam Number")
+        dropMenu = tk.OptionMenu(self, self.Beam, *BeamNum)
+        dropMenu.grid(row = 2, column = 1)
+    #Parameters can be stored in self.Beam, can change its variable type
+    ############################################################################
+
+    #Drop down menu for beam type for the given beam number
+    ############################################################################
+        BeamTypes = ['LG Beam', 'HG Beam', 'Bessel']
+        self.BeamT = tk.StringVar()
+        self.BeamT.set("Beam Type")
+        dropMenu = tk.OptionMenu(self, self.BeamT, *BeamTypes)
+        dropMenu.grid(row = 3, column = 1)
+    ############################################################################
+
+        label = tk.Label(self, text ="P value")
+        label.grid(row = 4, column = 0, padx = 30)
+
+        self.pVal = tk.StringVar()
+        myEntry = tk.Entry(self, textvariable = self.pVal)
+        myEntry.grid(row = 4, column = 1, padx = 10, pady = 10, sticky = "E")
+
+    ############################################################################
+
+        label = tk.Label(self, text ="L value")
+        label.grid(row = 5, column = 0, padx = 30)
+
+        self.LVal = tk.StringVar()
+        myEntry = tk.Entry(self, textvariable = self.pVal)
+        myEntry.grid(row = 5, column = 1, padx = 10, pady = 10, sticky = "E")
+
+    ############################################################################
 
 app = ControlScript()
 app.mainloop()
