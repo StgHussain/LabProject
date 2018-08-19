016f32921b3bf7686de99353075a2cfd35d35872
Alessandro Barbosa <alessandrodsbarbosa@gmail.com>
2018-08-19 21:17:36 +0200

    non-working Stash for branch change
    Needing to change branch, stashing for now

--- v3 python/Laguerre.py
+++ v3 python/Laguerre.py
@@ -1,7 +1,7 @@
 import time
 import numpy as np
 import cupy as cp
-from numba import cuda
+from numba import cuda, vectorize, float32
 import math
 
 class Laguerre():
@@ -10,31 +10,31 @@ class Laguerre():
             print("laguerre beam")
 
         def LaguerreBeam(self, p, l, x):
-            Vals = cp.zeros((p+1, 1))
+            Vals = np.zeros((p+1, 1), dtype=float)
             if p == 0:
                 Vals = Vals + 1
             else:
                 for m in range (p+1):
                     factM1 = math.factorial(p + l)
-                    numerator = (cp.power(-1, m)) * factM1
+                    numerator = (np.power(-1, m)) * factM1
                     factPM = math.factorial(p - m)
                     factLM = math.factorial(l + m)
                     factM2 = math.factorial(m)
                     denom = factPM * factLM * factM2
                     Vals[p - m][0] = (numerator/denom)
             #polyVal if needed
-            Results = cp.zeros(len(x), len(x[0]))
+            #Results = np.zeros((len(x), len(x[0])))
             ### GPU Implementation ###
-            PolyCoeff = self.PolyVal(Vals, x, Results)
+            #PolyCoeff = self.PolyVal(Vals, x, Results) 
             ### END ###
-            #PolyCoeff = cp.polyval(Vals, x)
+            PolyCoeff = np.polyval(Vals, x)
             return PolyCoeff
         
-        @cuda.jit
+        @vectorize
         def Add(self, A, B):
             return A + B
 
-        @cuda.jit
+        @vectorize
         def Multiply(self, A, B):
             return A * B
 
